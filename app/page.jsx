17fe84f198abaaac325899d00106709e6b398003
zhai <panyu.zhai@clife.cn>
2024-07-17 22:35:30 +0800

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,29 @@
+import LikeButton from "./like-button";
+
+// import { useState } from 'react';
+function Header({ title }) {
+  return <h1>{title ? title : "Default title"}</h1>;
+}
+
+export default function HomePage() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+
+  // const [likes, setLikes] = useState(0);
+
+  // function handleClick() {
+  //   setLikes(likes + 1);
+  // }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship." />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <LikeButton />
+    </div>
+  );
+}
